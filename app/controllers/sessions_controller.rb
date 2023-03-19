ef2a7da49df25858df030e21b1bcde8372a7e89a
KingL0ui5 <liulouis1@gmail.com>
2023-03-19 15:15:35 +0000

    Basic Login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,17 @@ class SessionsController < ApplicationController
   def create 
     user = User.find_by(EmailAddress: params[:session][:EmailAddress])
     if user && user.authenticate(params[:session][:password])
+      log_in(user)
+      redirect_to root_path
       #login and direct to users show page
     else
       flash[:danger] = 'Invalid email or password'
       render 'new'
     end
   end
-  
+
   def destroy 
+    log_out
+    redirect_to root_path 
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,20 @@
 module SessionsHelper
+  def log_in(user) #logs in user 
+    session[:user_EmployeeID] = user.id
+  end
+  
+  def current_user #gets current user 
+    if session[:user_EmployeeID]
+      @current_user ||= User.find_by(EmployeeID: session[:user_EmployeeID])
+    end  
+  end
+  
+  def logged_in?
+    !current_user.nil?
+  end
+  
+  def log_out
+    session.delete(:user_EmployeeID)
+    @current_user = nil
+  end   
 end
