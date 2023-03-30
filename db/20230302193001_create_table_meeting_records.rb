3578f69f225e562bbe501542c3c69e8d31743689
KingL0ui5 <liulouis1@gmail.com>
2023-03-30 21:04:54 +0000

    main

--- db/20230302193001_create_table_meeting_records.rb
+++ db/20230302193001_create_table_meeting_records.rb
@@ -1,17 +1,17 @@
 class CreateTableMeetingRecords < ActiveRecord::Migration[5.0]
   def change
-    create_table :MeetingRecords, id: :integer do |t|
+    create_table :meetingrecords, primary_key: 'meeting_id' do |t|
       t.string :zoom_meeting_id, null: false
       t.datetime :start_time, null: false 
       t.integer :duration, null: false
       t.string :type, null: false 
-      t.references :user, foreign_key: true #host
-      t.references :department, foreign_key: true #participants
+      t.references :department, foreign_key: true, index: true #participants
+      t.integer :EmployeeID #host
+      
+      t.index :EmployeeID
+      t.foreign_key :users, column: :EmployeeID, primary_key: :EmployeeID
+      
       t.timestamps
     end
-    
-    execute <<-SQL
-      ALTER TABLE meetingRecords ADD PRIMARY KEY (MeetingID);
-    SQL
   end
 end
